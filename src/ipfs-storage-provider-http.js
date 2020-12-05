4eba370fdf82d9e3e35b6ad68e648d725c87a741
Tim Göller <tim.goeller@posteo.de>
2020-12-05 13:14:23 +0100

    set storage type

--- src/ipfs-storage-provider-http.js
+++ src/ipfs-storage-provider-http.js
@@ -3,6 +3,10 @@ const { globSource } = IpfsHttpClient
 const ipfs = IpfsHttpClient()
 
 class IPFSStorageProviderHTTP {
+  constructor () {
+    this.type = 'ipfs'
+  }
+
   store (localFolder) {
     return ipfs.add(globSource(localFolder, { recursive: true, pin: true })).then((data) => {
       return data.cid.toString()
